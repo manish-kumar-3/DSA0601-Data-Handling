84207ad66b7140a039bfd8913a46f0aa6d5e4481
B. Manishkumar <112232600+manish-kumar-3@users.noreply.github.com>
2023-11-09 21:12:42 +0530

    Create linegraph.r

---
+++ linegraph.r
@@ -0,0 +1,9 @@
+# Sample data
+x_values <- c(1, 2, 3, 4, 5)
+y_values <- c(10, 15, 7, 12, 9)
+
+# Plotting the line graph
+plot(x_values, y_values, type='l', col='blue', lwd=2, pch=16, xlab='X-axis Label', ylab='Y-axis Label', main='Line Graph ')
+
+# Adding grid
+grid()
