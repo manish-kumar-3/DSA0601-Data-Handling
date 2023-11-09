7d95122956f33fbeec46f5d7080c1216a35c7336
B. Manishkumar <112232600+manish-kumar-3@users.noreply.github.com>
2023-11-09 20:08:58 +0530

    Create boxplot.r

---
+++ boxplot.r
@@ -0,0 +1,10 @@
+input <- mtcars[, c('mpg', 'cyl')]
+print(head(input))
+# Load the dataset
+data(mtcars)
+
+# Create the box plot
+boxplot(disp ~ gear, data = mtcars,
+		main = "Displacement by Gear",
+		xlab = "Gear",
+		ylab = "Displacement")
